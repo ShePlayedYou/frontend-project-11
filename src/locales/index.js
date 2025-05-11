01b767c008363557c833110a9745bbacba91cbe7
Nastya <kota1213@mail.ru>
2025-05-11 21:32:29 +0300

    Updated localization with i18next

---
+++ src/locales/index.js
@@ -0,0 +1,14 @@
+const resources = {
+    ru: {
+      translation: {
+        form: {
+          invalid: 'Ссылка должна быть валидным URL',
+          notUnique: 'RSS уже существует',
+          success: 'RSS успешно загружен',
+          required: 'Поле обязательно для заполнения',
+        }
+      }
+    },
+  };
+  
+  export default resources;
---
+++ src/yupLocale.js
@@ -0,0 +1,10 @@
+import * as Yup from 'yup';
+
+Yup.setLocale({
+  mixed: {
+    required: () => ({ key: 'form.required' }),
+  },
+  string: {
+    url: () => ({ key: 'form.invalid' }),
+  }
+});
